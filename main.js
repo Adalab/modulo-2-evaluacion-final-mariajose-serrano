74d2f61a126111d5ddd511fdc86d78e408766991
Maria Jose Serrano Higueras <mariajoseserranohigueras@MacBook-Air-de-Maria-Jose.local>
2025-11-12 20:32:10 +0100

    css modified items list

--- main.js
+++ main.js
@@ -2,3 +2,5 @@
 
 // SECCIÓN DE QUERY-SELECTOR
 // Éstos son los elementos que nos traemos de la página HTML y usamos en el código
+
+const js_products_list = document.querySelector(".js_products-list");
