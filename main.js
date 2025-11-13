4647cfd410df25e3b3925fe87def212418ab5d7e
Maria Jose Serrano Higueras <mariajoseserranohigueras@MacBook-Air-de-Maria-Jose.local>
2025-11-13 11:55:07 +0100

    bringing all products from API

--- main.js
+++ main.js
@@ -17,5 +17,24 @@ const normalize = (s) =>
     .normalize("NFD")
     .replace(/[\u0300-\u036f]/g, "");
 
-//Funcion para aplicar filtro
-//BUSCAR
+// Ahora hacer EL FETCH A la API
+
+// --- Función para mostrar los productos ---
+
+// Escucha el input para filtrar
+
+// --- Traer todos los productos de la API ---
+//Esta es la función que llama a la API Y nos devuelve products
+const getProducts = async () => {
+  const response = await fetch("https://fakestoreapi.com/products");
+  const products = await response.json(); // ← aquí obtienes el array completo
+  console.log(products); // Muestra el array completo en consola
+  return products;
+};
+
+// --- Ejemplo de uso hemos llamado a get products ---
+getProducts().then((products) => {
+  // llamamos y despuiés lo logeamos, Aquí ya tengo el array de ptos disponible
+
+  console.log("Productos recibidos:", products);
+});
